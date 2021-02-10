190beac0a33b538142b6010e082757fdda1a69f6
Moshe Fromer <mofromer@gmail.com>
2021-02-10 13:48:46 +0200

    display repo data

--- backend/github/views.py
+++ backend/github/views.py
@@ -1,3 +1,52 @@
-from django.shortcuts import render
+import re
 
-# Create your views here.
+import requests
+from django.http import JsonResponse
+from requests.auth import HTTPBasicAuth
+from rest_framework.decorators import api_view
+
+from backend.settings import GITHUB_API_TOKEN
+from github.constants import GITHUB_URL
+
+
+def get(url):
+    return requests.get(url, auth=HTTPBasicAuth('mo.fromer@gmail.com',GITHUB_API_TOKEN))
+
+
+def get_page_count(response):
+    try:
+        return re.findall('page=([\d]*)>; rel="last"', response.headers['Link'])[0]
+    except KeyError:
+        return 0
+
+
+@api_view(['GET'])
+def repos(request):
+    response = get(f'{GITHUB_URL}/repositories')
+    data = response.json()[:2]
+    response_data = []
+    for x in data:
+        repo_url = x['url']
+        repo_info = get(repo_url).json()
+        tags_response = get(repo_info['tags_url']).json()
+        tags = [x['name'] for x in tags_response]
+        contibutors_response = get(f"{repo_info['contributors_url']}?per_page=1")
+        contibutors_count = get_page_count(contibutors_response)
+        branches_response = get(f'{repo_url}/branches/master').json()
+        last_commit_date = branches_response['commit']['commit']['committer']['date']
+        pulls_url = f'{repo_url}/pulls?per_page=1'
+        pulls_count = get_page_count(get(pulls_url))
+        required_fields = {
+            'name': x['name'],
+            'description': x['description'],
+            'last_commit_date': last_commit_date,
+            'contributor_count': contibutors_count,
+            'open_issues': repo_info['open_issues'],
+            'open_prs':pulls_count,
+            'number_of_stars': repo_info['stargazers_count'],
+            'languages': repo_info['language'],
+            'tags':tags,
+        }
+        response_data.append(required_fields)
+    response_data = {'repos': response_data}
+    return JsonResponse(response_data)
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,8 +1,68 @@
 
+import React,{useState} from 'react';
+import {Button} from '@material-ui/core';
+import axios from 'axios';
+import ReactTable from 'react-table-v6';
+import 'react-table-v6/react-table.css';
+const BASE_URL = 'http://localhost:8000/github/';
+const columns = [
+  {
+      Header: 'Name',
+      accessor: 'name',
+  },
+  {
+      Header: 'Description',
+      accessor: 'description',
+  },
+  {
+      Header: 'Last commit date',
+      accessor: 'last_commit_date',
+  },
+  {
+      Header: 'Count of contributors',
+      accessor: 'contributor_count',
+  },
+  {
+      Header: 'Count of open and closed issues',
+      accessor: 'open_issues',
+  },
+  {
+      Header: 'Count of open Pull Requests',
+      accessor: 'open_prs',
+  },
+  {
+      Header: 'Number of stars',
+      accessor: 'number_of_stars',
+  },
+  {
+      Header: 'Programming languages used in the repository',
+      accessor: 'languages',
+  },
+  {
+      Header: 'Tags',
+      accessor: 'tags',
+  },
+
+]
+
 function App() {
+  const [repos, setRepos] = useState([]);
+  const fetchRepos = async() => {
+    const response = await axios.get(`${BASE_URL}repos/`);
+    setRepos(response.data.repos)
+    console.log(response.data.repos[0])    
+  }
   return (
     <div className="App">
-      Init
+      <Button onClick={fetchRepos}>
+        Fetch
+      </Button>
+      <ReactTable
+        title="Repos"
+        data={repos}
+        columns={columns}
+        defaultPageSize={100}
+      />   
     </div>
   );
 }
