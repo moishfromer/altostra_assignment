4dafd77006c2a24c2ee4097d1079af06b074d256
Moshe Fromer <mofromer@gmail.com>
2021-02-11 02:35:29 +0200

    add search functionality

--- backend/github/views.py
+++ backend/github/views.py
@@ -9,8 +9,8 @@ from backend.settings import GITHUB_API_TOKEN
 from github.constants import GITHUB_URL
 
 
-def get(url):
-    return requests.get(url, auth=HTTPBasicAuth('mo.fromer@gmail.com',GITHUB_API_TOKEN))
+def get(url, **kwargs):
+    return requests.get(url, auth=HTTPBasicAuth('mo.fromer@gmail.com',GITHUB_API_TOKEN), **kwargs)
 
 
 def get_page_count(response):
@@ -22,32 +22,46 @@ def get_page_count(response):
 
 @api_view(['GET'])
 def repos(request):
-    num_of_repos = 5
-    response = get(f'{GITHUB_URL}/repositories')
-    data = response.json()[:num_of_repos]
+    query_string = ' '.join(f'{key}:{value}' for key, value in request.GET.items() if value)
+    if not query_string:
+        return JsonResponse({'message': 'need to enter query'}, status=400)
+    params = {
+        'per_page': 5,
+        'q': query_string
+    }
+    response = get(f'{GITHUB_URL}/search/repositories', params=params)
+    data = response.json()['items']
     response_data = []
-    for x in data:
-        repo_url = x['url']
-        repo_info = get(repo_url).json()
+    for repo_info in data:
         tags_response = get(repo_info['tags_url']).json()
-        tags = [x['name'] for x in tags_response]
+        tags = ', '.join(x['name'] for x in tags_response)
         contibutors_response = get(f"{repo_info['contributors_url']}?per_page=1")
         contibutors_count = get_page_count(contibutors_response)
-        branches_response = get(f'{repo_url}/branches/master').json()
-        last_commit_date = branches_response['commit']['commit']['committer']['date']
-        pulls_url = f'{repo_url}/pulls?per_page=1'
-        pulls_count = get_page_count(get(pulls_url))
+        last_commit_date = repo_info['pushed_at']
+        pulls_response = get(f'{repo_info["url"]}/pulls?per_page=1')
+        pulls_count = get_page_count(pulls_response)
+        languages_response = get(repo_info['languages_url']).json()
+        languages = ', '.join(x for x in languages_response.keys())
         required_fields = {
-            'name': x['name'],
-            'description': x['description'],
+            'name': repo_info['name'],
+            'description': repo_info['description'],
             'last_commit_date': last_commit_date,
             'contributor_count': contibutors_count,
             'open_issues': repo_info['open_issues'],
             'open_prs':pulls_count,
             'number_of_stars': repo_info['stargazers_count'],
-            'languages': repo_info['language'],
+            'languages': languages,
             'tags':tags,
         }
         response_data.append(required_fields)
     response_data = {'repos': response_data}
     return JsonResponse(response_data)
+
+
+@api_view(['GET'])
+def licenses(request):
+    response = get(f'{GITHUB_URL}/licenses')
+    licenses = response.json()
+    data = {'licenses': [x['key'] for x in licenses]}
+    return JsonResponse(data)
+
--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,12 @@
 
-import React,{useState} from 'react';
-import {Button} from '@material-ui/core';
+import React,{useState, useEffect} from 'react';
+import {Button, TextField,Grid, Card, CardContent, CircularProgress, FormControl, InputLabel,Select, MenuItem} from '@material-ui/core';
 import axios from 'axios';
 import ReactTable from 'react-table-v6';
 import 'react-table-v6/react-table.css';
-const BASE_URL = 'http://localhost:8000/github/';
+
+
+const BASE_URL = 'http://localhost:8000/github';
 const columns = [
   {
       Header: 'Name',
@@ -23,7 +25,7 @@ const columns = [
       accessor: 'contributor_count',
   },
   {
-      Header: 'Count of open and closed issues',
+      Header: 'Count of open issues',
       accessor: 'open_issues',
   },
   {
@@ -46,23 +48,131 @@ const columns = [
 ]
 
 function App() {
+  const [licenses, setLicenses] = useState([]);
   const [repos, setRepos] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [query, setQuery] = useState({
+    stars: '',
+    forks:'',
+    pushed: '',
+    license: '',
+    language: '',
+  })
+
+  useEffect(() => {
+    const fetchLicenses = async () => {
+      const response = await axios.get(`${BASE_URL}/licenses/`);
+      setLicenses(response.data.licenses);
+    };
+ 
+    fetchLicenses();
+  }, []);
+
   const fetchRepos = async() => {
-    const response = await axios.get(`${BASE_URL}repos/`);
+    setLoading(true)
+    const response = await axios.get(`${BASE_URL}/repos/`, {params:query});
     setRepos(response.data.repos)
-    console.log(response.data.repos[0])    
+    setLoading(false)
   }
+
+  const handleChange = ({target}) => {
+    setQuery({
+      ...query,
+      [target.name]: target.value,
+    })
+    console.log(query)
+  }
+
+  const isEmptyQuery = () => !Object.values(query).some(x=>x)
+
   return (
     <div className="App">
-      <Button onClick={fetchRepos}>
-        Fetch
-      </Button>
-      <ReactTable
-        title="Repos"
-        data={repos}
-        columns={columns}
-        defaultPageSize={100}
-      />   
+      <form>
+        <Card  style={{margin: '10px'}}>
+          <CardContent>
+            <Grid container alignItems="flex-end" spacing={2}>
+              <Grid item xs={12} sm={2}>
+                <TextField
+                  id="stars"
+                  name="stars"
+                  label="With this many stars"
+                  placeholder=">1000"
+                  value={query.stars}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={2}>
+                <TextField
+                  id="forks"
+                  name="forks"
+                  label="With this many forks"
+                  placeholder="50..100, <5"
+                  value={query.forks}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={2}>
+
+                <TextField
+                  id="pushed"
+                  name="pushed"
+                  label="commit date"
+                  placeholder="<YYYY-MM-DD"
+                  value={query.pushed}
+                  onChange={handleChange}
+                  fullWidth
+                />
+                </Grid>
+              <Grid item xs={12} sm={2}>
+                <FormControl fullWidth>
+                  <InputLabel htmlFor="license">With this license</InputLabel>
+                  <Select
+                    value={query.license}
+                    onChange={handleChange}
+                    inputProps={{
+                      name: 'license',
+                      id: 'license',
+                    }}
+                  >
+                    {licenses.map(license => <MenuItem key={license} value={license}>{license}</MenuItem>)}
+                  </Select>
+                </FormControl>
+                </Grid>
+              <Grid item xs={12} sm={2}>
+                <TextField
+                  id="language"
+                  name="language"
+                  label="With this language"
+                  placeholder="Any language"
+                  value={query.language}
+                  onChange={handleChange}
+                  fullWidth
+                />
+              </Grid>
+              <Grid item xs={12} sm={2}>
+              
+                <Button disabled={loading || isEmptyQuery()} onClick={fetchRepos} variant="contained" color="primary">
+                  {loading ? <CircularProgress size={25}/> : 'Fetch'}
+
+                </Button>
+              </Grid>
+            </Grid>
+          </CardContent>
+        </Card>
+      </form>
+      <Card style={{margin: '10px'}}>
+        <CardContent>
+          <ReactTable
+            title="Repos"
+            data={repos}
+            columns={columns}
+            defaultPageSize={10}
+            showPagination={false}
+          />   
+        </CardContent>
+      </Card>
     </div>
   );
 }
